56f5997f24616f3f5aebb80e6510f3ebd0fee26d
XZDXRZ <13918165843@163.com>
2019-03-23 20:11:37 +0800

    重新开始

--- backend.cpp
+++ backend.cpp
@@ -8,11 +8,11 @@
 #include <cstring>
 #include <cmath>
 #include "map.hpp"
+#define MAPN 0
 using namespace std;
 char ch;//玩家输入
 int px,py,health;
 int z_count;
-extern char _map[MAXN][MAXN];
 class Zombie {
     public:
         int id,x,y;
@@ -22,8 +22,8 @@ class Zombie {
             a:x=rand()%10;
             y=rand()%10;
             id=arg_id;
-            if (_map[x][y]==' ') {
-                _map[x][y]='Z';
+            if (_map[MAPN][x][y]==' ') {
+                _map[MAPN][x][y]='Z';
                 dead=false;
             } else
                 goto a;
@@ -33,55 +33,55 @@ class Zombie {
         void move() {
             int tx,ty;
             //僵尸攻击机制
-            if (_map[x+1][y]=='I')
+            if (_map[MAPN][x+1][y]=='I')
                 health--;
-            if (_map[x-1][y]=='I')
+            if (_map[MAPN][x-1][y]=='I')
                 health--;
-            if (_map[x][y+1]=='I')
+            if (_map[MAPN][x][y+1]=='I')
                 health--;
-            if (_map[x][y-1]=='I')
+            if (_map[MAPN][x][y-1]=='I')
                 health--;
             srand((short)time(NULL));
             tx=rand()%2;
             ty=rand()%2;
-            if (_map[x+tx][y+ty]!=' ') {
+            if (_map[MAPN][x+tx][y+ty]!=' ') {
                 tx*=-1;
                 ty*=-1;
             }
-            if (_map[x+tx][y+ty]==' ') {
-                _map[x][y]=' ';
+            if (_map[MAPN][x+tx][y+ty]==' ') {
+                _map[MAPN][x][y]=' ';
                 x+=tx;
                 y+=ty;
-                _map[x][y]='Z';
+                _map[MAPN][x][y]='Z';
                 return;
             }
         }
         void close_attack() {//近身攻击
             if (abs(px-x)<=5 && abs(py-y)<=5) {//判断与玩家的距离
                 if (px-x<=0) {//在玩家上
-                    if (_map[x-1][y]==' ') {
-                        _map[x][y]=' ';
+                    if (_map[MAPN][x-1][y]==' ') {
+                        _map[MAPN][x][y]=' ';
                         x--;
-                        _map[x][y]='Z';
+                        _map[MAPN][x][y]='Z';
                     }
                 } else {
-                    if (_map[x+1][y]==' ') {
-                        _map[x][y]=' ';
+                    if (_map[MAPN][x+1][y]==' ') {
+                        _map[MAPN][x][y]=' ';
                         x++;
-                        _map[x][y]='Z';
+                        _map[MAPN][x][y]='Z';
                     }
                 }
                 if (py-y>=0) {//在玩家左
-                    if (_map[x][y+1]==' ') {
-                        _map[x][y]=' ';
+                    if (_map[MAPN][x][y+1]==' ') {
+                        _map[MAPN][x][y]=' ';
                         y++;
-                        _map[x][y]='Z';
+                        _map[MAPN][x][y]='Z';
                     }
                 } else {
-                    if (_map[x][y-1]==' ') {
-                        _map[x][y]=' ';
+                    if (_map[MAPN][x][y-1]==' ') {
+                        _map[MAPN][x][y]=' ';
                         y--;
-                        _map[x][y]='Z';
+                        _map[MAPN][x][y]='Z';
                     }
                 }
             }
@@ -90,7 +90,7 @@ class Zombie {
 bool is_win() {
     for (int i=0;i<=100;i++)
         for (int j=0;j<=100;j++)
-            if (_map[i][j]=='Z')
+            if (_map[MAPN][i][j]=='Z')
                 return false;
     return true;
 }
@@ -103,7 +103,7 @@ void count_zombies() {//统计僵尸数量
     z_count=0;
     for (int i=0;i<=MAXN;i++) {//遍历地图每一个点
         for (int j=0;j<=MAXN;j++) {
-            if (_map[i][j]=='Z')
+            if (_map[MAPN][i][j]=='Z')
                 z_count++;
         }
     }
@@ -112,55 +112,55 @@ void move(int lines) {
     //玩家移动
     ch = getch();
     if (ch=='w'||ch=='W') {
-        if (_map[px-1][py]==' ') {
-            _map[px][py]=' ';
+        if (_map[MAPN][px-1][py]==' ') {
+            _map[MAPN][px][py]=' ';
             px--;
-            _map[px][py]='I';
+            _map[MAPN][px][py]='I';
         }
     }
     if (ch=='a'||ch=='A') {
-        if (_map[px][py-1]==' ') {
-            _map[px][py]=' ';
+        if (_map[MAPN][px][py-1]==' ') {
+            _map[MAPN][px][py]=' ';
             py--;
-            _map[px][py]='I';
+            _map[MAPN][px][py]='I';
         }
     }
     if (ch=='s'||ch=='S') {
-        if (_map[px+1][py]==' ') {
-            _map[px][py]=' ';
+        if (_map[MAPN][px+1][py]==' ') {
+            _map[MAPN][px][py]=' ';
             px++;
-            _map[px][py]='I';
+            _map[MAPN][px][py]='I';
         }
     }
     if (ch=='d'||ch=='D') {
-        if (_map[px][py+1]==' ') {
-            _map[px][py]=' ';
+        if (_map[MAPN][px][py+1]==' ') {
+            _map[MAPN][px][py]=' ';
             py++;
-            _map[px][py]='I';
+            _map[MAPN][px][py]='I';
         }
     }
     //这块地方是玩家的攻击机制
     if (ch==' ') {
-        if (_map[px-1][py]=='Z') {
-            _map[px-1][py]=' ';
+        if (_map[MAPN][px-1][py]=='Z') {
+            _map[MAPN][px-1][py]=' ';
             is_dead(px,py);
         }
-        if (_map[px][py-1]=='Z') {
-            _map[px][py-1]=' ';
+        if (_map[MAPN][px][py-1]=='Z') {
+            _map[MAPN][px][py-1]=' ';
             is_dead(px,py);
         }
-        if (_map[px+1][py]=='Z') {
-            _map[px+1][py]=' ';
+        if (_map[MAPN][px+1][py]=='Z') {
+            _map[MAPN][px+1][py]=' ';
             is_dead(px,py);
         }
-        if (_map[px][py+1]=='Z') {
-            _map[px][py+1]=' ';
+        if (_map[MAPN][px][py+1]=='Z') {
+            _map[MAPN][px][py+1]=' ';
             is_dead(px,py);
         }
     }
     system("cls");
     for (int i=0;i<=lines;i++)
-        puts(_map[i]);
+        puts(_map[MAPN][i]);
 }
 void mode1(int lines) {//多线程式（整合）
     HANDLE hConsole;
@@ -186,7 +186,7 @@ void mode1(int lines) {//多线程式（整合）
     Sleep(100);
     system("cls");
     for (int i=0;i<=lines;i++)
-        puts(_map[i]);
+        puts(_map[MAPN][i]);
     while(true){//游戏主循环
         if(_kbhit())
 			move(13);
@@ -202,7 +202,7 @@ void mode1(int lines) {//多线程式（整合）
 		}
 		system("cls");
 		for (int i=0;i<=lines;i++)
-            puts(_map[i]);
+            puts(_map[MAPN][i]);
         count_zombies();
 		printf("Aline Time: %d\n",num);
 		printf("Your Blood: %d\n",health);
